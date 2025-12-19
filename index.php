2c2d7f5753e8ca2b14325381b25200a34a05b2ad
ayebareamon31-sketch <ayebareamon31@gmail.com>
2025-12-19 11:00:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
-<head><title>Campus WiFi</title></head>
+<head>
+    <title>Campus WiFi Portal</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
 <body>
-<h2>Bishop Barham University College Kabale</h2>
 
-<a href="register.php">Create Admin Account</a><br>
-<a href="login.php">Admin Login</a>
+<div class="auth-box">
+    <h2>Bishop Barham University College Kabale</h2>
+    <p>Campus WiFi Management System</p>
+
+    <hr><br>
+
+    <a href="register.php">
+        <button>Create Admin Account</button>
+    </a>
+    <br><br>
+    <a href="login.php">
+        <button>Admin Login</button>
+    </a>
+</div>
+
 </body>
 </html>
+
+
